2f7cefa7a473006b61f02fbee8b763df337ec451
bp3s <shibsankarmanna447@gmail.com>
2022-04-30 17:48:07 +0530

    first commit

---
+++ home.php
@@ -0,0 +1,12 @@
+<?php 
+session_start();
+
+if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+
+    header("location: index1.html");
+}
+else{
+     header("Location: index.php");
+     exit();
+}
+ ?>
